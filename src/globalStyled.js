b3ee75e72a4cedebc66bb28db093638dc21143f7
noobless <nuzulzen@gmail,com>
2020-12-27 14:03:21 +0700

    add GlobalStyled

---
+++ src/globalStyled.js
@@ -0,0 +1,10 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Kanit', sans-serif;
+}
+`;
