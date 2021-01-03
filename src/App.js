614e58adfce92755b62f74c5a3cf843c5153141e
noobless <nuzulzen@gmail,com>
2021-01-03 17:32:46 +0700

    add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
- 
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
+import { GlobalStyled } from "./globalStyled";
+import Hero from "./components/Hero";
+import Products from "./components/Products";
+import { productData, productDataTwo } from "./components/Products/data";
+import Feature from "./components/Features";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
+    <Router>
+      <GlobalStyled />
+      <Hero />
+      <Products heading="Choose your favorite" data={productData} />
+      <Feature />
+      <Products heading="Sweet Treats for You" data={productDataTwo} />
+      <Footer />
+    </Router>
   );
 }
 
