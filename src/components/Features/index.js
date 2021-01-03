2bf6473e57b3c1a8f6833fd7c2ce9802031fe550
noobless <nuzulzen@gmail,com>
2021-01-03 17:32:03 +0700

    add Feature, Product and Footer Compts

---
+++ src/components/Features/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { FeatureContainer, FeatureButton } from "./FeatureElements";
+
+function Feature() {
+  return (
+    <FeatureContainer>
+      <h1>Pizza of the Day</h1>
+      <p>Truffe alfred sauce topped with 24 carot gold dust</p>
+      <FeatureButton>Order Now</FeatureButton>
+    </FeatureContainer>
+  );
+}
+
+export default Feature;
